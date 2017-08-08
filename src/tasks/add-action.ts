700b79b71da24999f058d4471d505828c95f42e6
danielnikravesh <devcrazier@gmail.com>
2017-08-08 22:05:39 +0200

    Make first unit test for add-action task green.

--- src/tasks/add-action.ts
+++ src/tasks/add-action.ts
@@ -1,6 +1,12 @@
 import * as path from 'path';
 import * as ts from 'typescript';
-import {Decorator, Modifier, ReadonlyToken, Statement, SyntaxKind, Token, TypeElement} from 'typescript';
+import {
+    Identifier,
+    Modifier, NodeArray, NodeFlags, ObjectLiteralExpression, Statement, SyntaxKind, TypeAliasDeclaration, TypeElement,
+    TypeNode,
+    UnionTypeNode,
+    VariableStatement
+} from 'typescript';
 
 export const createInterface = (name: string): Statement => {
     const propModifiers: Modifier[] = [
@@ -42,30 +48,106 @@ export const createActionCreator = (name: string) => {
         ts.createBlock([stmtReturn], true));
     const declaration = ts.createVariableDeclaration(actionCreateName, undefined, expr);
     const stmActionCreator = ts.createVariableStatement(
-        [ts.createToken(SyntaxKind.ExportKeyword), ts.createToken(SyntaxKind.ConstKeyword)],
-        [declaration]
+        [ts.createToken(SyntaxKind.ExportKeyword)],
+        ts.createVariableDeclarationList([declaration], NodeFlags.Const)
     );
     return stmActionCreator;
 };
+
+const createActionTypeConstants = (name: string) => {
+    const expr = ts.createLiteral(name)
+    const declaration = ts.createVariableDeclaration(name, undefined, expr);
+    const stmConstant = ts.createVariableStatement(
+        [ts.createToken(SyntaxKind.ExportKeyword)],
+        ts.createVariableDeclarationList([declaration], NodeFlags.Const)
+    );
+    return stmConstant;
+};
+
+const createOrUpdateActionUnionTypeDeclaration = (name: string, oldStmt?: Statement) => {
+    const stmt = !!oldStmt ? oldStmt : ts.createTypeAliasDeclaration([],
+        [ts.createToken(SyntaxKind.ExportKeyword)],
+        'Action',
+        [],
+        ts.createUnionTypeNode([]));
+
+    if (stmt.kind === SyntaxKind.TypeAliasDeclaration) {
+        const typeAliasDeclaration = <TypeAliasDeclaration> stmt;
+        const type = typeAliasDeclaration.type;
+
+        let unionTypes: NodeArray<TypeNode> = ts.createNodeArray();
+
+        if (type.kind === SyntaxKind.UnionType) {
+            const unionType = (<UnionTypeNode> type);
+            unionTypes = unionType.types;
+        } else if (type.kind === SyntaxKind.TypeReference) {
+            unionTypes.push(type);
+        }
+        unionTypes.push(ts.createTypeReferenceNode(name, []));
+
+        const updatedUnionType = ts.createUnionTypeNode(unionTypes);
+        return ts.updateTypeAliasDeclaration(
+            typeAliasDeclaration,
+            [],
+            [ts.createToken(SyntaxKind.ExportKeyword)],
+            typeAliasDeclaration.name,
+            [],
+            updatedUnionType);
+    }
+
+    return stmt;
+};
+
+const createOrUpdateActionTypesAssignment = (name: string, actionTypeConstantName: string, oldStmt?: Statement) => {
+    const declaration = ts.createVariableDeclaration('ActionTypes', undefined, ts.createObjectLiteral());
+    const stmt = !!oldStmt ?
+        oldStmt
+        : ts.createVariableStatement([], ts.createVariableDeclarationList([declaration], NodeFlags.Const));
+
+    if (stmt.kind === SyntaxKind.VariableStatement) {
+        const variableStatement = <VariableStatement> stmt;
+        const variableDeclaration = variableStatement.declarationList.declarations[0];
+        if (variableDeclaration && variableDeclaration.initializer &&
+            variableDeclaration.initializer.kind === SyntaxKind.ObjectLiteralExpression) {
+            const objectLiteralExpr = <ObjectLiteralExpression> variableDeclaration.initializer;
+            const properties = objectLiteralExpr.properties;
+
+            const newProperty = ts.createPropertyAssignment(
+                name,
+                ts.createTypeAssertion(
+                    ts.createTypeQueryNode(ts.createIdentifier(actionTypeConstantName)),
+                    ts.createIdentifier(actionTypeConstantName)));
+            properties.push(newProperty)
+        }
+    }
+
+    return stmt;
+};
+
+export const isActionUnionType = (stmt: Statement): boolean =>
+    stmt.kind === SyntaxKind.TypeAliasDeclaration && (<TypeAliasDeclaration> stmt).name.text === 'Action';
+
+export const isActionTypesAssignment = (stmt: Statement): boolean =>
+    stmt.kind === SyntaxKind.VariableStatement && (<Identifier>(<VariableStatement> stmt).declarationList.declarations[0].name).text === 'ActionTypes';
 
 export const addAction = (code: string) => {
     const originalSourceFile = ts.createSourceFile("target.ts", code, ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
     const resultFile = ts.createSourceFile(path.join(__dirname, "someFileName.ts"), "", ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
 
-    var newStatements = [];
-
-    for (var i = 0; i < originalSourceFile.statements.length; i++) {
-        const oldStatement = originalSourceFile.statements[i];
-        console.log(JSON.stringify(oldStatement));
-
-        //console.log(oldStatement.kind === ts.SyntaxKind.TypeAssertionExpression);
-        //if(oldStatement.identifier) {}
-        newStatements.push(oldStatement);
-    }
+    let newStatements = originalSourceFile.statements
+        .filter(stmt => !isActionUnionType(stmt) && !isActionTypesAssignment(stmt));
 
     const nameNewAction = 'ActionTestName';
+    const qualifiedActionType = `SOME_QUALIFIED_PREFIX_${nameNewAction}`;
+    newStatements.push(createActionTypeConstants(qualifiedActionType));
     newStatements.push(createInterface(nameNewAction));
     newStatements.push(createActionCreator(nameNewAction));
+
+    const actionUnionTypeDeclaration = originalSourceFile.statements.find(stm => isActionUnionType(stm));
+    newStatements.push(createOrUpdateActionUnionTypeDeclaration(nameNewAction, actionUnionTypeDeclaration));
+
+    const actionTypesAssignment = originalSourceFile.statements.find(stm => isActionTypesAssignment(stm));
+    newStatements.push(createOrUpdateActionTypesAssignment(nameNewAction, qualifiedActionType, actionTypesAssignment));
 
     const sourceFile = ts.updateSourceFileNode(originalSourceFile, newStatements);
 
--- test/example01.test.ts
+++ test/example01.test.ts
@@ -1,7 +1,7 @@
 import {addAction} from '../src/tasks/add-action';
 
 const code = 'export interface Action01 {\n' +
-    '    type: typeof ActionTypes.ACTION_01;\n'+
+    '    type: typeof ActionTypes.ACTION_01;\n' +
     '}' +
     '\n' +
     'export type Action = Action01;' +
@@ -9,7 +9,7 @@ const code = 'export interface Action01 {\n' +
     'const ACTION_01 = "ACTION_01";' +
     '\n' +
     'export const ActionTypes = {\n' +
-    '    ACTION_01: typeof <ACTION_01>ACTION_01\n' +
+    '    ACTION_01: <typeof ACTION_01> ACTION_01\n' +
     '};\n' +
     'export const createAction01 = (): Action01 => {\n' +
     '    return {\n' +
@@ -18,28 +18,28 @@ const code = 'export interface Action01 {\n' +
     '};\n';
 
 const expectedResult = 'export interface Action01 {\n' +
-    '}' +
-    '\n' +
-    'export interface Action02 {\n' +
-    '}' +
-    '\n' +
-    'export type Action = Action01 | Action02;' +
-    '\n' +
-    'const ACTION_01 = "ACTION_01";' +
-    '\n' +
-    'export const ActionTypes = {\n' +
-    '    ACTION_01: typeof <ACTION_01>ACTION_01\n' +
-    '};\n' +
+    '    type: typeof ActionTypes.ACTION_01;\n' +
+    '}\n' +
+    'const ACTION_01 = "ACTION_01";\n' +
     'export const createAction01 = (): Action01 => {\n' +
-    '    return {};\n' +
+    '    return {\n' +
+    '        type: ActionTypes.ACTION_01\n' +
+    '    };\n' +
     '};\n' +
-    'export const createAction02 = (): Action02 => {\n' +
-    '    return {};\n' +
+    'export const SOME_QUALIFIED_PREFIX_ActionTestName = "SOME_QUALIFIED_PREFIX_ActionTestName";\n' +
+    'interface ActionTestName {\n' +
+    '    readonly type: typeof ActionTypes.ActionTestName;\n' +
+    '}\n' +
+    'export const createActionTestName = (): ActionTestName => {\n' +
+    '    return { type: ActionTypes.ActionTestName };\n' +
+    '};\n' +
+    'export type Action = Action01 | ActionTestName;\n' +
+    'export const ActionTypes = {\n' +
+    '    ACTION_01: <typeof ACTION_01>ACTION_01,\n' +
+    '    ActionTestName: (<typeof SOME_QUALIFIED_PREFIX_ActionTestName>SOME_QUALIFIED_PREFIX_ActionTestName)\n' +
     '};\n';
 
 test('AddAction', () => {
     const modifiedCode = addAction(code);
-    expect(modifiedCode).toEqual(code);
-    
-    console.log(modifiedCode);
+    expect(modifiedCode).toEqual(expectedResult);
 });

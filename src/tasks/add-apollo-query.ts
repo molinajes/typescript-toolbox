718a14a42c4342d6575fd6d9c21b1d0ee3061c17
danielnikravesh <devcrazier@gmail.com>
2017-08-13 22:34:50 +0200

    Update task 'add-apollo-query' to support containers which already have another apollo-query.

--- src/tasks/add-apollo-query.ts
+++ src/tasks/add-apollo-query.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as ts from 'typescript';
 import {
+    CallExpression, Expression,
     Identifier,
     ImportDeclaration, NamedImports, NodeArray, NodeFlags, Statement, StringLiteral, SyntaxKind,
     TypeAliasDeclaration, TypeNode, TypeReferenceNode, UnionOrIntersectionTypeNode, VariableStatement
@@ -8,7 +9,7 @@ import {
 import {capitalizeFirstLetter, convertHyphensToCamelCase, removeFileExtension} from '../utils/string-utils';
 import {createImport, createNamespaceImport} from '../utils/ts-utils';
 import {componentPropsInterfaceName, containerAllPropsTypeName} from './add-container';
-import {addWithReduxConstant, isAllPropsType, isComponentConstant} from './add-redux';
+import {isAllPropsType, isComponentConstant} from './add-redux';
 
 export const apolloPropsTypeName = 'ApolloProps';
 export const withApolloConstantName = 'withApollo';
@@ -17,6 +18,9 @@ export const getDataPropTypeName = (name: string) => `${capitalizeFirstLetter(na
 
 export const addApolloImport = (): Statement =>
     createImport([{element: 'graphql'}, {element: 'DefaultChildProps'}], 'react-apollo');
+
+export const addComposeImport = (): Statement =>
+    createImport([{element: 'compose'}], 'redux');
 
 export const addGraphQlFilePath = (queryName: string, graphQlFilePath: string): Statement =>
     createNamespaceImport(queryName, graphQlFilePath);
@@ -150,6 +154,39 @@ export const addWithApolloConstant = (queryName: string) => {
         ts.createVariableDeclarationList([declaration], NodeFlags.Const)
     );
 };
+export const updateWithApolloConstant = (oldStmt: VariableStatement, queryName: string) => {
+    const oldVariableDeclaration = oldStmt.declarationList.declarations[0];
+    const oldExpr = oldStmt.declarationList.declarations[0].initializer;
+
+    const expr = ts.createCall(
+        ts.createIdentifier('graphql'),
+        undefined,
+        [
+            ts.createIdentifier(queryName)
+        ]
+    );
+
+    let newExpr: Expression = undefined;
+
+    if (oldExpr.kind === SyntaxKind.CallExpression
+        && (<CallExpression> oldExpr).expression.kind === SyntaxKind.Identifier
+        && (<Identifier> (<CallExpression> oldExpr).expression).text === 'compose') {
+        const composeCallExpr = oldExpr as CallExpression;
+        const oldComposeArgs = (<CallExpression> oldExpr).arguments;
+        const newComposeArgs = oldComposeArgs.concat([expr]);
+        newExpr = ts.updateCall(composeCallExpr, composeCallExpr.expression, composeCallExpr.typeArguments, newComposeArgs);
+    } else {
+        newExpr = ts.createCall(ts.createIdentifier('compose'), undefined, [oldExpr, expr]);
+    }
+
+    const declaration = ts.updateVariableDeclaration(oldVariableDeclaration, oldVariableDeclaration.name, oldVariableDeclaration.type, newExpr);
+    const declarationList = ts.updateVariableDeclarationList(oldStmt.declarationList, [declaration]);
+    return ts.updateVariableStatement(
+        oldStmt,
+        [ts.createToken(SyntaxKind.ExportKeyword)],
+        declarationList
+    );
+};
 
 export const updateComponentConstantWithApollo = (oldStatement: VariableStatement): Statement => {
     const decl = oldStatement.declarationList.declarations[0];
@@ -165,8 +202,13 @@ export const updateComponentConstantWithApollo = (oldStatement: VariableStatemen
     return ts.updateVariableStatement(oldStatement, oldStatement.modifiers, updatedVariableDeclarationList);
 };
 
+export const isComposeImport = (stmt: Statement) =>
+    stmt.kind === SyntaxKind.ImportDeclaration && (<ImportDeclaration> stmt).importClause.namedBindings.kind === SyntaxKind.NamedImports
+    && (<NamedImports>(<ImportDeclaration> stmt).importClause.namedBindings).elements.some(e => e.name.text === 'compose');
+
 export const isQueryDataModel = (stmt: Statement) =>
     stmt.kind === SyntaxKind.TypeAliasDeclaration && (<TypeAliasDeclaration> stmt).name.text.endsWith('Query');
+
 
 export const getQueryDataModelTypeName = (stmts: Statement[]): string | undefined => {
     const stmt = stmts.find(isQueryDataModel);
@@ -184,6 +226,12 @@ export const getQueryVariablesTypeName = (stmts: Statement[]): string | undefine
 export const isApolloPropsType = (stmt: Statement): boolean =>
     stmt && stmt.kind === SyntaxKind.TypeAliasDeclaration && (<TypeAliasDeclaration> stmt).name.text === apolloPropsTypeName;
 
+export const isWithApolloConstant = (stmt: Statement): boolean =>
+    stmt && stmt.kind === SyntaxKind.VariableStatement
+    && (<VariableStatement> stmt).declarationList.declarations[0]
+    && (<VariableStatement> stmt).declarationList.declarations[0].name.kind === SyntaxKind.Identifier
+    && (<Identifier> (<VariableStatement> stmt).declarationList.declarations[0].name).text === withApolloConstantName;
+
 export const addApolloQuery = (code: string, graphQlFilePath: string, queryName: string, queryModelsFilePath: string, queryModelsCode: string, componentName: string) => {
     // TODO: Use real path?
     const originalSourceFile = ts.createSourceFile("container.ts", code, ts.ScriptTarget.Latest, false, ts.ScriptKind.TS);
@@ -193,6 +241,9 @@ export const addApolloQuery = (code: string, graphQlFilePath: string, queryName:
     const queryVariablesTypeName = getQueryVariablesTypeName(queryModelsSourceFile.statements);
 
     const apolloPropsExist = originalSourceFile.statements.some(isApolloPropsType);
+    const withApolloConstantExists = originalSourceFile.statements.some(isWithApolloConstant);
+    const needComposeImport = withApolloConstantExists;
+    const composeImportExists = originalSourceFile.statements.some(isComposeImport);
     let newStatements = [];
 
     const addGraphQlImports = !originalSourceFile.statements.some(isApolloImport);
@@ -211,21 +262,33 @@ export const addApolloQuery = (code: string, graphQlFilePath: string, queryName:
             if (queryModelsTypeName || queryVariablesTypeName) {
                 newStatements.push(addQueryModelImport(queryModelsTypeName, queryVariablesTypeName, queryModelsFilePath));
             }
+
+            if (needComposeImport && !composeImportExists) {
+                newStatements.push(addComposeImport());
+            }
             addedImports = true;
         }
-        else if (apolloPropsExist && isApolloPropsType(statement)) {
+        else if (isApolloPropsType(statement)) {
+            newStatements.push(createQueryType(queryName, queryModelsTypeName));
             newStatements.push(updateApolloPropsTypeDeclaration(statement as TypeAliasDeclaration, queryName));
         }
         else if (isAllPropsType(statement)) {
-            newStatements.push(createQueryType(queryName, queryModelsTypeName));
             if (!apolloPropsExist) {
+                newStatements.push(createQueryType(queryName, queryModelsTypeName));
                 newStatements.push(updateApolloPropsTypeDeclaration(undefined, queryName));
             }
             newStatements.push(updateAllPropsTypeDeclaration(statement as TypeAliasDeclaration));
         }
+        else if (isWithApolloConstant(statement)) {
+            newStatements.push(updateWithApolloConstant(statement as VariableStatement, queryName));
+        }
         else if (isComponentConstant(statement, componentName)) {
-            newStatements.push(addWithApolloConstant(queryName));
-            newStatements.push(updateComponentConstantWithApollo(statement as VariableStatement));
+            if (!withApolloConstantExists) {
+                newStatements.push(addWithApolloConstant(queryName));
+                newStatements.push(updateComponentConstantWithApollo(statement as VariableStatement));
+            } else {
+                newStatements.push(statement);
+            }
         }
         else {
             newStatements.push(statement);
--- test/tasks/add-apollo-query.test.ts
+++ test/tasks/add-apollo-query.test.ts
@@ -55,3 +55,64 @@ test('addApolloQuery', () => {
     const modifiedCode = addApolloQuery(containerCode, './query/data-query.graphql', 'dataQuery', './query/data-query-models.ts', queryModelsCode, 'MyComponent');
     expect(modifiedCode).toEqual(expectedResult);
 });
+
+test('addApolloQuery to container with apollo query', () => {
+    const containerCode = 'import { MyComponent as MyComponentComponent } from "./my-component-component";\n' +
+        'import { connect, Disptach } from "react-redux";\n' +
+        'import { graphql, DefaultChildProps } from "react-apollo";\n' +
+        'import * as dataQuery from "./query/data-query.graphql";\n' +
+        'import { DataQuery, DataQueryVariables } from "./query/data-query-models.ts";\n' +
+        'export interface ReduxStateProps {\n' +
+        '}\n' +
+        'export interface ReduxDispatchProps {\n' +
+        '}\n' +
+        'export type DataQueryDataProp = DefaultChildProps<Props, DataQuery>;\n' +
+        'export type ApolloProps = DataQueryDataProp;\n' +
+        'export type AllProps = Props & ReduxStateProps & ReduxDispatchProps & ApolloProps;\n' +
+        'export const mapStateToProps = (): ReduxStateProps => {\n' +
+        '    return {};\n' +
+        '};\n' +
+        'export const mapDispatchToProps = (): ReduxDispatchProps => {\n' +
+        '    return {};\n' +
+        '};\n' +
+        'export const withRedux = connect<ReduxStateProps, ReduxDispatchProps, Props>(mapStateToProps, mapDispatchToProps);\n' +
+        'export const withApollo = graphql(dataQuery);\n' +
+        'export const MyComponent = withApollo(withRedux(MyComponentComponent));\n';
+
+    const queryModelsCode = '\n' +
+        'export type OtherDataQueryVariables = {\n' +
+        '  id: string,\n' +
+        '};\n' +
+        '\n' +
+        'export type OtherDataQuery = {\n' +
+        '  element:  {\n' +
+        '    id: string\n' +
+        '  } | null\n' +
+        '};';
+    const expectedResult = 'import { MyComponent as MyComponentComponent } from "./my-component-component";\n' +
+        'import { connect, Disptach } from "react-redux";\n' +
+        'import { graphql, DefaultChildProps } from "react-apollo";\n' +
+        'import * as dataQuery from "./query/data-query.graphql";\n' +
+        'import { DataQuery, DataQueryVariables } from "./query/data-query-models.ts";\n' +
+        'import * as otherDataQuery from "./query/other-data-query.graphql";\n' +
+        'import { OtherDataQuery, OtherDataQueryVariables } from "./query/other-data-query-models.ts";\n' +
+        'import { compose } from "redux";\n' +
+        'export interface ReduxDispatchProps {\n' +
+        '}\n' +
+        'export type DataQueryDataProp = DefaultChildProps<Props, DataQuery>;\n' +
+        'export type OtherDataQueryDataProp = DefaultChildProps<Props, OtherDataQuery>;\n' +
+        'export type ApolloProps = DataQueryDataProp & OtherDataQueryDataProp;\n' +
+        'export type AllProps = Props & ReduxStateProps & ReduxDispatchProps & ApolloProps;\n' +
+        'export const mapStateToProps = (): ReduxStateProps => {\n' +
+        '    return {};\n' +
+        '};\n' +
+        'export const mapDispatchToProps = (): ReduxDispatchProps => {\n' +
+        '    return {};\n' +
+        '};\n' +
+        'export const withRedux = connect<ReduxStateProps, ReduxDispatchProps, Props>(mapStateToProps, mapDispatchToProps);\n' +
+        'export const withApollo = compose(graphql(dataQuery), graphql(otherDataQuery));\n' +
+        'export const MyComponent = withApollo(withRedux(MyComponentComponent));\n';
+
+    const modifiedCode = addApolloQuery(containerCode, './query/other-data-query.graphql', 'otherDataQuery', './query/other-data-query-models.ts', queryModelsCode, 'MyComponent');
+    expect(modifiedCode).toEqual(expectedResult);
+});
